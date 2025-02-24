f78a1268af325928da6beae740c725f3b4529f88
511NetworkAuthenticationRequired <143905058+511NetworkAuthenticationRequired@users.noreply.github.com>
2025-02-24 15:58:40 -0300

    Create 01. CalculadoraBasica.pas

---
+++ Ejercicios Resueltos/Guia 1/01. CalculadoraBasica.pas
@@ -0,0 +1,12 @@
+// 1) Escribir un programa en pascal que dado dos números enteros realice la suma, resta, multiplicación, división entera y resto de los mismos. Los numero son: X = 55 e Y = 30.
+program ej1;
+const
+    x = 55;
+    y = 30;
+begin
+    writeln('Suma: ', x + y);
+    writeln('Resta: ', x - y);
+    writeln('Multiplicacion: ', x * y);
+    writeln('Division: ', x div y);
+    writeln('Modulo: ', x mod y);
+end.
