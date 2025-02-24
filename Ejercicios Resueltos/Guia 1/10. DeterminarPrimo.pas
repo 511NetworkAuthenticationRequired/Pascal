951d4e3ef8a28563c507449cc921c3b077ff439c
511NetworkAuthenticationRequired <143905058+511NetworkAuthenticationRequired@users.noreply.github.com>
2025-02-24 16:11:28 -0300

    Create 10. DeterminarPrimo.pas

---
+++ Ejercicios Resueltos/Guia 1/10. DeterminarPrimo.pas
@@ -0,0 +1,21 @@
+// Determinar si un número es primo
+program ej10;
+var
+    num, aux: longint;
+    esPrimo: boolean;
+begin
+    writeln('Por favor ingrese un numero para saber si es entero:');
+    readln(num);
+    aux:= 2;
+    esPrimo:= true;
+    while (esPrimo = true) and (aux < num) do
+    begin
+        if (num mod aux = 0) then 
+            esPrimo:= false;
+        aux:= aux + 1;
+    end;
+    if (esPrimo = true) then
+        writeln('El numero ingresado es Primo.')
+    else
+        writeln('El numero ingresado no es primo.');
+end.
