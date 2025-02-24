3304d704d8fe2917b607acb7d69174b9ff2af4aa
511NetworkAuthenticationRequired <143905058+511NetworkAuthenticationRequired@users.noreply.github.com>
2025-02-24 16:02:17 -0300

    Update 01. CalculadoraBasica.pas

--- Ejercicios Resueltos/Guia 1/01. CalculadoraBasica.pas
+++ Ejercicios Resueltos/Guia 1/01. CalculadoraBasica.pas
@@ -1,5 +1,5 @@
 // 1) Escribir un programa en pascal que dado dos números enteros realice la suma, resta, multiplicación, división entera y resto de los mismos. Los numero son: X = 55 e Y = 30.
-program ej1;
+program CalculadoraBasica;
 const
     x = 55;
     y = 30;
